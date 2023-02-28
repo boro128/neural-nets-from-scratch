837f2b488cead9dcada1c0f25a5164f6c04f7da4
Mateusz Borowski <m.borowski128@gmail.com>
2023-02-28 13:42:09 +0100

    Add getter

--- mm/activation/sigmoid.py
+++ mm/activation/sigmoid.py
@@ -3,7 +3,11 @@ import numpy as np
 
 class Sigmoid:
     def __init__(self):
-        self.output = None
+        self._output = None
 
     def forward(self, inputs: np.ndarray) -> None:
-        self.output = 1 / (1 + np.exp(-inputs))
+        self._output = 1 / (1 + np.exp(-inputs))
+
+    @property
+    def output(self):
+        return self._output
--- mm/layer_input.py
+++ mm/layer_input.py
@@ -3,7 +3,11 @@ import numpy as np
 
 class LayerInput:
     def __init__(self):
-        self.output = None
+        self._output = None
 
     def forward(self, inputs: np.ndarray):
-        self.output = inputs
+        self._output = inputs
+
+    @property
+    def output(self):
+        return self._output
