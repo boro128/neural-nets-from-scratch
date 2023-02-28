65db6f99a3a5fef85efa673485b7966903e111de
Mateusz Borowski <m.borowski128@gmail.com>
2023-02-28 11:18:59 +0100

    Add more examples

--- examples/1_2_1_square_simple.py
+++ examples/1_2_1_square_simple.py
@@ -9,6 +9,8 @@ df_test = pd.read_csv("datasets/regression/square-simple-test.csv", index_col=0)
 X = df_test["x"].values.reshape((-1, 1))
 y = df_test["y"].values
 
+# two hidden layers, 5 neurons each
+
 model = mm.MLP()
 
 layer1 = mm.Layer_Dense(1, 5)
@@ -18,20 +20,20 @@ layer1.bias = np.array([[-1.7032547, -1.7720956, -9.60833, 15.706666, 7.0029044]
 layer2 = mm.Layer_Dense(5, 5)
 layer2.weights = np.array(
     [
-        [-5.2239532, -7.603729, -12.939046, -4.4141293, -4.2426047],
-        [-4.315536, -9.46816, -5.326138, 1.6418235, 1.119893],
-        [-8.629273, -6.896841, -9.22218, -10.107484, -9.763134],
-        [1.4223001, 6.7677526, 4.9157257, 12.381927, 14.164914],
-        [6.4638653, 5.800516, 10.31827, 0.04026163, 0.64547026],
+        [-5.223953, -7.603729, -12.939046, -4.4141293, -4.2426046],
+        [-4.315546, -9.46816, -5.326138, 1.6418245, 1.119893],
+        [-8.629373, -6.896841, -9.22218, -10.107484, -9.763134],
+        [1.4223001, 6.7677526, 4.9257257, 12.381927, 14.164914],
+        [6.4638653, 5.800526, 10.31827, 0.04026163, 0.64547027],
     ]
 )
-layer2.bias = np.array([[-4.2617025, -2.02823, -2.5729601, 0.59431577, 1.9611949]])
+layer2.bias = np.array([[-4.2617027, -2.02823, -2.5729602, 0.59431577, 1.9611947]])
 
 layer3 = mm.Layer_Dense(5, 1)
 layer3.weights = np.array(
-    [[-73.4189], [-77.798546], [-83.767365], [-50.89138], [-53.15941]]
+    [[-73.41892], [-77.798546], [-83.767465], [-50.89238], [-53.15942]]
 )
-layer3.bias = np.array([[202.8993]])
+layer3.bias = np.array([[202.89932]])
 
 model.add(layer1)
 model.add(mm.activation.Sigmoid())
@@ -42,4 +44,89 @@ model.add(layer3)
 out = model.forward(X).squeeze()
 
 mse = np.mean(np.square(out - y))
-print(mse)
+print(f"two hidden layers, 5 neurons each, mse: {mse}")
+
+# one hidden layer, 10 neurons
+
+model = mm.MLP()
+
+layer1 = mm.Layer_Dense(1, 10)
+layer1.weights = np.array(
+    [
+        [
+            -9.673959,
+            -0.21904892,
+            -11.088262,
+            6.121494,
+            11.075849,
+            -9.836935,
+            -7.7007814,
+            14.651039,
+            -9.578117,
+            -9.422528,
+        ]
+    ]
+)
+layer1.bias = np.array(
+    [
+        9.307574,
+        7.2209636,
+        8.329594,
+        4.2570133,
+        -16.679883,
+        4.2632492,
+        -9.840837,
+        -26.55505,
+        11.508097,
+        11.084183,
+    ]
+)
+
+layer2 = mm.Layer_Dense(10, 1)
+layer2.weights = np.array(
+    [
+        [-30.793924],
+        [57.446616],
+        [-32.344653],
+        [-84.36993],
+        [91.28587],
+        [-30.9672420],
+        [134.886172],
+        [92.6416244],
+        [-34.245793],
+        [-33.797376],
+    ]
+)
+layer2.bias = np.array([59.41757])
+
+model.add(layer1)
+model.add(mm.activation.Sigmoid())
+model.add(layer2)
+
+out = model.forward(X).squeeze()
+
+mse = np.mean(np.square(out - y))
+print(f"one hidden layer, 10 neurons, mse: {mse}")
+
+# one hidden layer, 5 neurons
+
+model = mm.MLP()
+
+layer1 = mm.Layer_Dense(1, 5)
+layer1.weights = np.array([[-8.54567, -11.664024, -13.445912, -19.833573, -8.429798]])
+layer1.bias = np.array([-8.5419, 14.2041, 21.19816, 36.544794, 6.554697])
+
+layer2 = mm.Layer_Dense(5, 1)
+layer2.weights = np.array(
+    [[158.760499], [-82.37453], [-94.95956], [-71.802744], [-79.4415]]
+)
+layer2.bias = np.array([213.4618])
+
+model.add(layer1)
+model.add(mm.activation.Sigmoid())
+model.add(layer2)
+
+out = model.forward(X).squeeze()
+
+mse = np.mean(np.square(out - y))
+print(f"one hidden layer, 5 neurons, mse: {mse}")
--- examples/1_2_1_steps_large.py
+++ examples/1_2_1_steps_large.py
@@ -9,29 +9,30 @@ df_test = pd.read_csv("datasets/regression/steps-large-test.csv", index_col=0)
 X = df_test["x"].values.reshape((-1, 1))
 y = df_test["y"].values
 
+
+# two hidden layers, 5 neurons each
+
 model = mm.MLP()
 
 layer1 = mm.Layer_Dense(1, 5)
-layer1.weights = np.array([[-27.626356, -20.89344, 27.120974, 45.69689, 11.612845]])
-layer1.bias = np.array([[41.357834, -10.339673, 13.470915, -23.172596, 5.6102886]])
+layer1.weights = np.array([[-213.6800, -92.7280, 168.5175, -177.1530, -226.7014]])
+layer1.bias = np.array([-106.9620, 45.5648, -254.1388, 88.0355, -113.4121])
 
 layer2 = mm.Layer_Dense(5, 5)
 layer2.weights = np.array(
     [
-        [-8.3284931e00, -4.4820315e-01, -3.0572968e01, 4.7598863e00, -3.6684063e01],
-        [-1.5244807e00, 8.1501192e-01, -2.4369020e00, 2.3618015e01, -2.3995929e00],
-        [-1.9370271e00, 6.7774868e-01, 4.2990065e00, -2.9325153e01, 4.3877707e00],
-        [3.7552052e01, 3.2916479e00, 3.6218293e00, -3.6952205e00, 5.3370337e00],
-        [-5.3912705e-01, 1.2272812e-02, 4.9252028e00, -1.2176819e01, 5.8789783e00],
+        [-2.5420, -3.0759, -18.7080, 32.8780, -2.2470],
+        [-2.7019, -11.7106, -7.8789, -8.2232, -8.5836],
+        [40.1129, 7.0093, 6.7858, -3.6190, 42.5549],
+        [-3.3904, -22.3619, -14.2373, -9.7289, -5.4421],
+        [-2.6380, -2.1709, -14.0111, 34.8498, -1.8948],
     ]
 )
-layer2.bias = np.array([[-4.7462187, -0.85367996, 1.7170361, 2.4019506, 1.8899131]])
+layer2.bias = np.array([-29.9995, 10.8776, 8.8823, -14.7422, -7.5004])
 
 layer3 = mm.Layer_Dense(5, 1)
-layer3.weights = np.array(
-    [[58.631855], [37.152008], [35.596027], [-80.74089], [43.72223]]
-)
-layer3.bias = np.array([[-13.367186]])
+layer3.weights = np.array([[32.6226], [41.4524], [38.7555], [-79.7586], [47.5424]])
+layer3.bias = np.array([-0.0759])
 
 model.add(layer1)
 model.add(mm.activation.Sigmoid())
@@ -42,4 +43,87 @@ model.add(layer3)
 out = model.forward(X).squeeze()
 
 mse = np.mean(np.square(out - y))
-print(mse)
+print(f"two hidden layers, 5 neurons each, mse: {mse}")
+
+# one hidden layer, 10 neurons
+
+model = mm.MLP()
+
+layer1 = mm.Layer_Dense(1, 10)
+layer1.weights = np.array(
+    [
+        [
+            104.78577,
+            29.103115,
+            50.641045,
+            69.73865,
+            75.232544,
+            111.34952,
+            71.25317,
+            -112.89121,
+            -113.42035,
+            42.566055,
+        ]
+    ]
+)
+layer1.bias = np.array(
+    [
+        -52.28355,
+        -43.72382,
+        -76.04331,
+        -34.773716,
+        -112.92934,
+        -55.56218,
+        -106.96068,
+        -56.31029,
+        -56.574696,
+        -21.233469,
+    ]
+)
+
+layer2 = mm.Layer_Dense(10, 1)
+layer2.weights = np.array(
+    [
+        [48.358549],
+        [-43.259393],
+        [14.0381965],
+        [7.373916],
+        [58.136595],
+        [56.532244],
+        [50.598963],
+        [-39.926784],
+        [-40.555776],
+        [-32.40343],
+    ]
+)
+layer2.bias = np.array([0.27252027])
+
+model.add(layer1)
+model.add(mm.activation.Sigmoid())
+model.add(layer2)
+
+out = model.forward(X).squeeze()
+
+mse = np.mean(np.square(out - y))
+print(f"one hidden layer, 10 neurons, mse: {mse}")
+
+# one hidden layer, 5 neurons
+
+model = mm.MLP()
+
+layer1 = mm.Layer_Dense(1, 5)
+layer1.weights = np.array([[131.7981, 95.4253, 70.8011, 89.6914, 104.9499]])
+layer1.bias = np.array([65.7796, -47.5908, -106.3184, -134.6479, -52.3460])
+
+layer2 = mm.Layer_Dense(5, 1)
+layer2.weights = np.array([[80.0541], [35.3729], [33.2226], [47.4418], [44.7309]])
+layer2.bias = np.array([-80.1959])
+
+model.add(layer1)
+model.add(mm.activation.Sigmoid())
+model.add(layer2)
+
+out = model.forward(X).squeeze()
+
+mse = np.mean(np.square(out - y))
+print(f"one hidden layer, 5 neurons, mse: {mse}")
