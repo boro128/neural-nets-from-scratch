2d6b6508c0ad4f1a2e68ca90120c026a8447b8b0
Mateusz Borowski <m.borowski128@gmail.com>
2023-02-28 10:58:47 +0100

    Update layer dense

--- mm/layer_dense.py
+++ mm/layer_dense.py
@@ -4,7 +4,7 @@ import numpy as np
 class Layer_Dense:
     def __init__(self, n_inputs, n_outputs) -> None:
         self.weights = np.ones((n_inputs, n_outputs))
-        self.bias = np.zeros((1, n_outputs))
+        self.bias = np.zeros((n_outputs))
 
     def forward(self, input) -> None:
         self.output = input @ self.weights + self.bias
