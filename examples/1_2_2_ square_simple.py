2c9474cb15d9fae946e32e523962e31a51d144cc
Mateusz Borowski <m.borowski128@gmail.com>
2023-03-06 16:25:41 +0100

    Add training example

--- examples/1_2_2_ square_simple.py
+++ examples/1_2_2_ square_simple.py
@@ -20,24 +20,62 @@ scaler_y = mm.preprocessing.StandardScaler()
 y_train = scaler_y.fit_transform(y_train)
 y_test_transf = scaler_y.transform(y_test)
 
+
+def execute(batch_size, n_epochs, print_every=1000, draw_weights=False):
+    model = mm.Model()
+
+    model.add(mm.LayerDense(1, 5))
+    model.add(mm.activation.Sigmoid())
+    model.add(mm.LayerDense(5, 5))
+    model.add(mm.activation.Sigmoid())
+    model.add(mm.LayerDense(5, 1))
+
+    model.set_optimizer(mm.optim.SGD())
+    model.set_loss(mm.loss.MSELoss())
+
+    model.init_weights_xavier()
+
+    model.train(
+        X_train,
+        y_train,
+        n_epochs=n_epochs,
+        batch_size=batch_size,
+        print_every=print_every,
+    )
+
+    out = model.forward(X_test)
+    out = scaler_y.reverse_transform(out)
+    mse = np.mean(np.square(out - y_test), axis=0).squeeze()
+    print(f"{batch_size=}, {n_epochs=} mse: {mse}")
+
+    if draw_weights:
+        model.draw_weights()
+
+
 # two hidden layers, 5 neurons each
+# batch_size=1
+execute(batch_size=1, n_epochs=3000, print_every=1000)
 
-model = mm.Model()
+# two hidden layers, 5 neurons each
+# batch_size=10
+execute(batch_size=10, n_epochs=20_000, print_every=5000)
 
-model.add(mm.LayerDense(1, 5))
-model.add(mm.activation.Sigmoid())
-model.add(mm.LayerDense(5, 5))
-model.add(mm.activation.Sigmoid())
-model.add(mm.LayerDense(5, 1))
+# two hidden layers, 5 neurons each
+# batch_size=whole dataset
+execute(batch_size=None, n_epochs=200_000, print_every=50_000)
 
-model.set_optimizer(mm.optim.SGD())
-model.set_loss(mm.loss.MSELoss())
-
-model.train(X_train, y_train, n_epochs=1000, batch_size=1)
-
-model.draw_weights()
-
-out = model.forward(X_test)
-out = scaler_y.reverse_transform(out)
-mse = np.mean(np.square(out - y_test), axis=0).squeeze()
-print(f"one hidden layer, 5 neurons, mse: {mse}")
+# output;
+# epoch: 1000  loss: 0.0007435554765755554
+# epoch: 2000  loss: 0.0003916476501218075
+# epoch: 3000  loss: 0.0002364691620495575
+# batch_size=1, n_epochs=3000 mse: 2.2696451764901493
+# epoch: 5000  loss: 0.0011185880847739214
+# epoch: 10000  loss: 0.0006738650584537937
+# epoch: 15000  loss: 0.00047418679088143877
+# epoch: 20000  loss: 0.0003630403401623947
+# batch_size=10, n_epochs=20000 mse: 4.02358382781651
+# epoch: 50000  loss: 0.0011057558290373797
+# epoch: 100000  loss: 0.0006602219401620602
+# epoch: 150000  loss: 0.0004680576691515503
+# epoch: 200000  loss: 0.0003554184532142622
+# batch_size=None, n_epochs=200000 mse: 4.017415325970267
