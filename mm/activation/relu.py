55b00edc92c793844edc9eead2ba2a171d0a5b88
Mateusz Borowski <m.borowski128@gmail.com>
2023-03-28 17:35:09 +0200

    Update ReLu

--- mm/activation/relu.py
+++ mm/activation/relu.py
@@ -9,4 +9,4 @@ class ReLu(Activation):
         self._output = np.maximum(inputs, 0)
 
     def backward(self, dvalues: np.ndarray) -> None:
-        self._dinputs = dvalues * np.where(self._dinputs <= 0, 0, 1)
+        self._dinputs = dvalues * np.where(self._inputs <= 0, 0, 1)
