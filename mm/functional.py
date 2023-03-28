d29a3c528cadf4ce79ada5b90e6ef3a4b723c238
Mateusz Borowski <m.borowski128@gmail.com>
2023-03-28 16:45:00 +0200

    Add Fscore

--- mm/functional.py
+++ mm/functional.py
@@ -4,3 +4,19 @@ import numpy as np
 def softmax(inputs: np.ndarray) -> np.ndarray:
     exp = np.exp(inputs)
     return exp / np.sum(exp)
+
+
+def fscore(inputs: np.ndarray, target: np.ndarray, label: int) -> float:
+    # Fscore = 2 * (precision * recall) / (precision + recall)
+
+    tp = np.sum((target == label) & (inputs == label))
+    fp = np.sum((target != label) & (inputs == label))
+    fn = np.sum((inputs != label) & (target == label))
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    return 2 * (precision * recall) / (precision + recall)
+
+
+def fscore_macro(inputs: np.ndarray, target: np.ndarray):
+    return np.mean([fscore(inputs, target, label) for label in np.unique(target)])
